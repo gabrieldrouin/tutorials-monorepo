b23253e08fdc5986945f9a0e99e6a8ae03dc00b0
Gabriel Drouin <info@gabrieldrouin.com>
2024-12-21 22:30:44 -0500

    feat: remove eslint and tsconfig, let prettier do its thing just fine

--- zachgoll-express/intro-express-middleware/index.mjs
+++ zachgoll-express/intro-express-middleware/index.mjs
@@ -1,12 +1,13 @@
-import express, { Express, Request } from "express";
-const app: Express = express();
+import express from "express";
+
+const app = express();
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
 
-app.get("/", (_req: Request, res: any) => {
+app.get("/", (req, res, next) => {
   res.send("<h1>hello</h1>");
   console.log("get");
 });
